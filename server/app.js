0275a29a4f883a19a2d30938e6e1ef5a693875ee
MalikAhsan-PC <malikahsan13@gmail.com>
2022-12-08 16:21:48 +0500

    setup express-graphql and added graphql in app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql',graphqlHTTP({
+
+}));
 
 app.listen(4000,function(){
     console.log("Server Listening on Port 400");
